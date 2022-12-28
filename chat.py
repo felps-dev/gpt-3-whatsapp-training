01fdfe4c49840469be290ebd28ff9cdaba9e19f9
Felipe Correa <eu@felps.dev>
2022-12-28 02:14:29 -0300

    Improve bot response and fix requirements

--- chat.py
+++ chat.py
@@ -15,22 +15,28 @@ model_engine = os.getenv("MODEL_ENGINE")
 # Defina o prompt que você deseja usar
 sujeito = input("Digite o nome do sujeito: ")
 
-accumulated_prompt = ""
+accumulated_prompt = (
+    f"A seguir uma conversa entre {sujeito} e {response_user}. {response_user}"
+    + f" é uma Inteligência artificial criada para imitar a pessoa {response_user}"
+    + ". Ele acredita que é real, e que está vivo, não sabe que foi criado para"
+    + f" simular as respostas, comportamentos e personalidade de {response_user}."
+    + "\n"
+)
+
 while True:
     prompt = str(input(f"{sujeito}: ")).strip()
-    accumulated_prompt += f"{sujeito}: {prompt}\\n{response_user}:"
+    accumulated_prompt += f"{sujeito}: {prompt}\n{response_user}:"
     response = openai.Completion.create(
         engine=model_engine,
         prompt=accumulated_prompt,
-        max_tokens=50,
-        temperature=0.80,
+        max_tokens=150,
+        temperature=0.9,
         top_p=1,
         frequency_penalty=1,
+        presence_penalty=0.6,
+        stop=[f"{response_user}:", f"{sujeito}:"],
     )
-    resposta = response["choices"][0]["text"].split(":")[0]
-    resposta = resposta.replace("\\n", "").strip()
-    resposta = resposta.replace("\\", "").strip()
-    resposta = resposta.replace(response_user, "").strip()
-    resposta = resposta.replace(sujeito, "").strip()
-    accumulated_prompt += f"{resposta}\\n"
-    print(f"Eu: {resposta}")
+    resposta = response["choices"][0]["text"]
+    accumulated_prompt += resposta
+    resposta = resposta.replace("\n", "")
+    print(f"{response_user}:{resposta}")
